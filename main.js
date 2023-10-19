c192bb36934d86bf7de3f88f2cf9b08667ffa284
chrisstrom1 <108539898+chrisstrom1@users.noreply.github.com>
2023-10-19 18:41:16 -0500

    Update main.js
    final3

--- main.js
+++ main.js
@@ -1,26 +1,18 @@
 const API_URL = "https://deckofcardsapi.com/api/deck";
 let deckId;
 
+// Generate random image URLs for the player and computer avatars
+document.getElementById("player-avatar").src = getRandomAvatarUrl();
+document.getElementById("computer-avatar").src = getRandomAvatarUrl();
+
 document.getElementById("play-button").addEventListener("click", startGame);
 
 function startGame() {
-    // Use the API to create a new deck and shuffle it.
-    fetch(`${API_URL}/new/shuffle/?deck_count=1`)
-        .then((response) => response.json())
-        .then((data) => {
-            deckId = data.deck_id;
-            drawCards();
-        })
-        .catch((error) => console.error("Error:", error));
-}
-
-function drawCards() {
-    // Use the API to draw one card for both the player and the computer.
-    fetch(`${API_URL}/${deckId}/draw/?count=2`)
-        .then((response) => response.json())
-        .then((data) => {
-            const playerCard = data.cards[0];
-            const computerCard = data.cards[1];
+    createAndShuffleDeck()
+        .then(() => drawCards())
+        .then((cards) => {
+            const playerCard = cards[0];
+            const computerCard = cards[1];
 
             // Display the cards on the UI.
             document.getElementById("player-card").innerHTML = `
@@ -36,17 +28,22 @@ function drawCards() {
         .catch((error) => console.error("Error:", error));
 }
 
-
-function getRandomAvatarUrl() {
-    const avatarUrls = [
-        "avatar1.jpg",
-        "avatar2.jpg",
-        "avatar3.jpg",
-        "avatar4.jpg",
-    ];
-    const randomIndex = Math.floor(Math.random() * avatarUrls.length);
-    return avatarUrls[randomIndex];
+function createAndShuffleDeck() {
+    // Use the API to create a new deck and shuffle it.
+    return fetch(`${API_URL}/new/shuffle/?deck_count=1`)
+        .then((response) => response.json())
+        .then((data) => {
+            deckId = data.deck_id;
+        });
 }
+
+function drawCards() {
+    // Use the API to draw one card for both the player and the computer.
+    return fetch(`${API_URL}/${deckId}/draw/?count=2`)
+        .then((response) => response.json())
+        .then((data) => data.cards);
+}
+
 function displayResult(playerCard, computerCard) {
     // Compare the card ranks to determine the round winner.
     let result = "";
@@ -87,4 +84,26 @@ function getCardRank(cardValue) {
     // Use the rank map to get the numerical rank of the card.
     // Convert the card value to uppercase to handle lowercase values.
     return rankMap[cardValue.toUpperCase()] || 0;
+}
+
+function getRandomAvatarUrl() {
+    // Generate random avatar URLs for player and computer
+    const playerAvatars = [
+        "player1.jpg",
+        "player2.jpg",
+        // Add more avatar URLs as needed
+    ];
+    const computerAvatars = [
+        "computer1.jpg",
+        "computer2.jpg",
+        // Add more avatar URLs as needed
+    ];
+    
+    const randomPlayerAvatar = playerAvatars[Math.floor(Math.random() * playerAvatars.length)];
+    const randomComputerAvatar = computerAvatars[Math.floor(Math.random() * computerAvatars.length)];
+
+    return {
+        player: randomPlayerAvatar,
+        computer: randomComputerAvatar,
+    };
 }
